49dc61aaeeea88c43a3d6db57cd03c9b935d8d56
ChaleGuo <hfut_gc@163.com>
2021-11-05 11:53:36 +0800

    Initial commit

---
+++ src/main/java/com/example/exercise/week1/MyClassLoader.java
@@ -0,0 +1,41 @@
+package com.example.exercise.week1;
+
+import lombok.SneakyThrows;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+
+public class MyClassLoader extends ClassLoader {
+
+    @SneakyThrows
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        FileInputStream fis = new FileInputStream("src\\main\\java\\com\\example\\exercise\\week1\\Hello.xlass");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = fis.read(buffer)) != -1) {
+            baos.write(buffer, 0, len);
+        }
+
+        byte[] byteData = baos.toByteArray();
+        fis.close();
+        baos.close();
+
+        for (int i = 0; i < byteData.length; i++) {
+            byteData[i] = (byte) (255 - byteData[i]);
+        }
+
+        return defineClass(name, byteData, 0, byteData.length);
+    }
+
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        Class<?> clazz = new MyClassLoader().findClass("Hello");
+        Method hello = clazz.getMethod("hello");
+        hello.invoke(clazz.getDeclaredConstructor().newInstance());
+    }
+}
